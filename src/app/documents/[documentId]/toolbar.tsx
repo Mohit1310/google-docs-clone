4dd41ec7cb43733c292bebde2709dfc288f5335d
Mohit1310 <mohitdayama1234@gmail.com>
2025-06-04 13:52:03 +0530

    feat: adding zustand and undo functionality

---
+++ src/app/documents/[documentId]/toolbar.tsx
@@ -0,0 +1,55 @@
+"use client";
+import { cn } from "@/lib/utils";
+import { useEditorStore } from "@/store/use-editor-store";
+import { type LucideIcon, Undo2Icon } from "lucide-react";
+
+interface ToolbarButtonProps {
+  onClick?: () => void;
+  isActive?: boolean;
+  icon: LucideIcon;
+}
+
+const ToolbarButton = ({
+  onClick,
+  isActive,
+  icon: Icon,
+}: ToolbarButtonProps) => {
+  return (
+    <button
+      onClick={onClick}
+      className={cn(
+        "flex h-7 min-w-7 items-center justify-center rounded-sm text-sm hover:bg-neutral-200/80",
+        isActive && "bg-neutral-200/80",
+      )}
+      type="button"
+    >
+      <Icon className="size-4" />
+    </button>
+  );
+};
+
+export const Toolbar = () => {
+  const { editor } = useEditorStore();
+
+  const sections: {
+    label: string;
+    icon: LucideIcon;
+    onClick: () => void;
+    isActive?: boolean;
+  }[][] = [
+    [
+      {
+        label: "Undo",
+        icon: Undo2Icon,
+        onClick: () => editor?.chain().focus().undo().run(),
+      },
+    ],
+  ];
+  return (
+    <div className="flex min-h-10 items-center gap-x-0.5 overflow-x-auto rounded-6 bg-[#f1f4f9] px-2.5 py-0.5">
+      {sections[0].map((item) => (
+        <ToolbarButton key={item.label} {...item} />
+      ))}
+    </div>
+  );
+};
