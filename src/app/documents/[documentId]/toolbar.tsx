80e772ef31dcad0f5a79432bdcee126eab274a68
Mohit1310 <mohitdayama1234@gmail.com>
2025-06-04 16:39:25 +0530

    refactor: toolbar

--- src/app/documents/[documentId]/toolbar.tsx
+++ src/app/documents/[documentId]/toolbar.tsx
@@ -1,7 +1,20 @@
 "use client";
+import { Separator } from "@/components/ui/separator";
 import { cn } from "@/lib/utils";
 import { useEditorStore } from "@/store/use-editor-store";
-import { type LucideIcon, Undo2Icon } from "lucide-react";
+import {
+  BoldIcon,
+  ItalicIcon,
+  ListTodoIcon,
+  type LucideIcon,
+  MessageSquarePlusIcon,
+  PrinterIcon,
+  Redo2Icon,
+  RemoveFormattingIcon,
+  SpellCheckIcon,
+  UnderlineIcon,
+  Undo2Icon,
+} from "lucide-react";
 
 interface ToolbarButtonProps {
   onClick?: () => void;
@@ -43,11 +56,107 @@ export const Toolbar = () => {
         icon: Undo2Icon,
         onClick: () => editor?.chain().focus().undo().run(),
       },
+      {
+        label: "Redo",
+        icon: Redo2Icon,
+        onClick: () => editor?.chain().focus().redo().run(),
+      },
+      {
+        label: "Print",
+        icon: PrinterIcon,
+        onClick: () => window.print(),
+      },
+      {
+        label: "Spell Check",
+        icon: SpellCheckIcon,
+        onClick: () => {
+          const current = editor?.getAttributes("spellcheck");
+          editor?.view.dom.setAttribute(
+            "spellcheck",
+            current ? "true" : "false",
+          );
+        },
+      },
+    ],
+    [
+      {
+        label: "Bold",
+        icon: BoldIcon,
+        isActive: editor?.isActive("bold"),
+        onClick: () => editor?.chain().focus().toggleBold().run(),
+      },
+      {
+        label: "Italic",
+        icon: ItalicIcon,
+        isActive: editor?.isActive("italic"),
+        onClick: () => editor?.chain().focus().toggleItalic().run(),
+      },
+      {
+        label: "Underline",
+        icon: UnderlineIcon,
+        isActive: editor?.isActive("underline"),
+        onClick: () => editor?.chain().focus().toggleUnderline().run(),
+      },
+    ],
+    [
+      {
+        label: "Comment",
+        icon: MessageSquarePlusIcon,
+        onClick: () => console.log("todo: comment"),
+        isActive: false,
+      },
+      {
+        label: "List Todo",
+        icon: ListTodoIcon,
+        onClick: () => editor?.chain().focus().toggleTaskList().run(),
+        isActive: editor?.isActive("taskList"),
+      },
+      {
+        label: "Remove Formatting",
+        icon: RemoveFormattingIcon,
+        onClick: () => editor?.chain().focus().unsetAllMarks().run(),
+      },
     ],
   ];
   return (
     <div className="flex min-h-10 items-center gap-x-0.5 overflow-x-auto rounded-6 bg-[#f1f4f9] px-2.5 py-0.5">
       {sections[0].map((item) => (
+        <ToolbarButton key={item.label} {...item} />
+      ))}
+      <Separator
+        orientation="vertical"
+        className="h-6 min-h-6 bg-neutral-300"
+      />
+      {/* TODO: Font family */}
+      <Separator
+        orientation="vertical"
+        className="h-6 min-h-6 bg-neutral-300"
+      />
+      {/* TODO: Heading */}
+      <Separator
+        orientation="vertical"
+        className="h-6 min-h-6 bg-neutral-300"
+      />
+      {/* TODO: Font size */}
+      <Separator
+        orientation="vertical"
+        className="h-6 min-h-6 bg-neutral-300"
+      />
+      {sections[1].map((item) => (
+        <ToolbarButton key={item.label} {...item} />
+      ))}
+      {/* TODO: Text color */}
+      {/* TODO: highlight color */}
+      <Separator
+        orientation="vertical"
+        className="h-6 min-h-6 bg-neutral-300"
+      />
+      {/* TODO: Link */}
+      {/* TODO: Image */}
+      {/* TODO: Align */}
+      {/* TODO: Line height */}
+      {/* TODO: List */}
+      {sections[2].map((item) => (
         <ToolbarButton key={item.label} {...item} />
       ))}
     </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,15 @@
+import Link from "next/link";
+
 const Home = () => {
   return (
     <div className="flex min-h-screen items-center justify-center">
-      <p>hello world</p>
+      <p>
+        Here is the{" "}
+        <Link href={"/documents/1234"} className="text-blue-500 underline">
+          link
+        </Link>{" "}
+        to the documents
+      </p>
     </div>
   );
 };
